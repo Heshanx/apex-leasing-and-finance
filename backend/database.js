25ff512fe1e6f6220fd0b0e8310bcfc5f7b83208
Susitha Ayodya <susitha0217@gmail.com>
2024-10-13 18:48:13 +0530

    signup Sarver

--- backend/database.js
+++ backend/database.js
@@ -4,16 +4,34 @@ const cors = require('cors');
 const bodyparser =require('body-parser');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000'})); //
 app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true })); 
 
 const db = mysql.createConnection({
-    host :'localhost',
+    host :'127.0.0.1',
     user :'root',
-    password : ' ',
+    password : '',
     database : 'signup'
-})
-
-app.listen(3000,()=>{
+});
+db.connect((err) => {
+    if (err) {
+        console.log('Error connecting to database:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+app.post('/signup',(req,res)=>{
+    const {cust_name,User_name,Password}= req.body;
+    const sql ="Insert into signup(cust_name,User_name,Password) Values(?,?,?)";
+    const values = [cust_name,User_name,Password];   
+    db.query(sql,values,(err,data)=>{
+        if(err){
+            return res.status(500).json({ error: err.message });
+        } 
+        res.status(201).json({ message: 'User signed up successfully', data });
+    });
+});
+app.listen(5000,()=>{
     console.log("Listning....");
-})
+});
